d1f16407f21c35078028846d8172bbb116748367
theobuzz14-cmyk <240716808+theobuzz14-cmyk@users.noreply.github.com>
2025-11-18 12:21:40 +0000

    [finish]chapter7

--- test/controllers/homes_controller_test.rb
+++ test/controllers/homes_controller_test.rb
@@ -1,8 +1,7 @@
 require "test_helper"
 
 class HomesControllerTest < ActionDispatch::IntegrationTest
-  test "should get top" do
-    get homes_top_url
-    assert_response :success
-  end
+  # test "the truth" do
+  #   assert true
+  # end
 end
