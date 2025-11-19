f4032b683e07b889ea88708ae216a0664f908efc
theobuzz14-cmyk <240716808+theobuzz14-cmyk@users.noreply.github.com>
2025-11-19 13:22:59 +0000

    [finish]chapter14

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   get 'homes/about', to: 'homes#about', as: 'about'
 
   resources :post_images, only: [:new, :create, :index, :show, :destroy]
+  resources :users, only: [:show, :edit]
 
 end
