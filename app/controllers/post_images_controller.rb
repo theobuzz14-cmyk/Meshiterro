84617a8b1973fc84a03b1d16cdd999268a536211
theobuzz14-cmyk <240716808+theobuzz14-cmyk@users.noreply.github.com>
2025-11-20 06:19:57 +0000

    [finish]chapter18

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -18,6 +18,7 @@ class PostImagesController < ApplicationController
 
   def show
     @post_image = PostImage.find(params[:id])
+    @post_comment = PostComment.new
   end
   
   def destroy
@@ -34,7 +35,6 @@ class PostImagesController < ApplicationController
   def post_image_params
     params.require(:post_image).permit(:shop_name, :image, :caption)
   end
-
 
 
 end
